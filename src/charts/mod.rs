24a79fb43103644b04bdc1a921a02ae37ad37364
Simon Kadlec <k12222898@students.jku.at>
2024-12-16 22:56:44 +0100

    added boxplot chart generation, deleted azure and openai integration, minor other changes

---
+++ src/charts/mod.rs
@@ -0,0 +1,102 @@
+use charming::{component::{Axis, Title}, datatype::{Dataset, Transform}, element::{
+    AxisType, SplitArea, SplitLine,
+}, series::{Boxplot}, Chart, HtmlRenderer, ImageRenderer};
+use charming::component::Legend;
+
+pub fn get_chart_multiple_dataset_4(data: Vec<Vec<f64>>, countries: Vec<&str>) {
+    let filtered_data: Vec<Vec<f64>> = data.into_iter()
+        .map(|inner_vec| inner_vec.into_iter().filter(|&x| x != 0.0).collect())
+        .collect();
+
+    let data0 = vec![filtered_data[0].clone()];
+    let data1 = vec![filtered_data[1].clone()];
+    let data2 = vec![filtered_data[2].clone()];
+    let data3 = vec![filtered_data[3].clone()];
+
+    let ds = Dataset::new()
+        .source(data0)
+        .source(data1)
+        .source(data2)
+        .source(data3)
+        .transform(
+            Transform::new()
+                .from_dataset_index(0)
+                .transform(r#"{"type": "boxplot"}"#),
+        )
+        .transform(
+            Transform::new()
+                .from_dataset_index(1)
+                .transform(r#"{"type": "boxplot"}"#),
+        )
+        .transform(
+            Transform::new()
+                .from_dataset_index(2)
+                .transform(r#"{"type": "boxplot"}"#),
+        )
+        .transform(
+            Transform::new()
+                .from_dataset_index(3)
+                .transform(r#"{"type": "boxplot"}"#),
+        );
+
+    let chart = Chart::new()
+        .title(Title::new().text("News Sentiment Boxplot").left("center"))
+        .dataset(ds)
+        .legend(Legend::new().top("4%"))
+        .x_axis(Axis::new()
+                    .type_(AxisType::Category)
+                    .boundary_gap(true)
+                    .name_gap(30)
+                    .split_area(SplitArea::new().show(false))
+                    .split_line(SplitLine::new().show(false)))
+        .y_axis(Axis::new()
+                    .type_(AxisType::Value)
+                    .name("sentiment value")
+                    .min(-1)
+                    .max(1)
+                    .interval(0.1)
+                    .split_area(SplitArea::new().show(true)), )
+        .series(Boxplot::new().name(countries[0]).dataset_index(4))
+        .series(Boxplot::new().name(countries[1]).dataset_index(5))
+        .series(Boxplot::new().name(countries[2]).dataset_index(6))
+        .series(Boxplot::new().name(countries[3]).dataset_index(7));
+
+    let mut renderer = HtmlRenderer::new("boxplot", 1000, 800);
+    renderer.save(&chart, "boxplot_multiple.html").expect("Failed to save chart");
+}
+
+pub fn get_chart_single_dataset(data: Vec<f64>, country: &str) {
+    let filtered_data: Vec<f64> = data.into_iter().filter(|&x| x != 0.0).collect();
+
+    let data0 = vec![filtered_data];
+
+    let ds = Dataset::new()
+        .source(data0)
+        .transform(
+            Transform::new()
+                .from_dataset_index(0)
+                .transform(r#"{"type": "boxplot"}"#),
+        );
+
+    let chart = Chart::new()
+        .title(Title::new().text("News Sentiment Boxplot").left("center"))
+        .dataset(ds)
+        .legend(Legend::new().top("4%"))
+        .x_axis(Axis::new()
+                    .type_(AxisType::Category)
+                    .boundary_gap(true)
+                    .name_gap(30)
+                    .split_area(SplitArea::new().show(false))
+                    .split_line(SplitLine::new().show(false)), )
+        .y_axis(Axis::new()
+                    .type_(AxisType::Value)
+                    .name("sentiment value")
+                    .min(-1)
+                    .max(1)
+                    .interval(0.1)
+                    .split_area(SplitArea::new().show(true)), )
+        .series(Boxplot::new().name(country).dataset_index(1));
+
+    let mut renderer = HtmlRenderer::new("boxplot", 1000, 800);
+    renderer.save(&chart, "boxplot_single.html").expect("Failed to save chart");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ mod austria;
 mod germany;
 mod france;
 mod translator;
-mod openai;
 mod article;
 mod wordcloud;
-mod azure_sentiment;
 mod vader_sentiment;
+mod charts;
 
+use rand::Rng;
 use text_io::read;
 use crate::article::NewsArticle;
-use crate::azure_sentiment::get_sentiment_scores;
 use crate::wordcloud::generate_wordcloud;
+use crate::charts::{get_chart_multiple_dataset_4, get_chart_single_dataset};
 
 fn main() {
     /*loop {
@@ -31,8 +31,26 @@ fn main() {
             _ => println!("Please try again!")
         }
     }*/
+
+    /*let data = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+    if let Err(e) = generate_boxplot(data, "boxplot.png") {
+        eprintln!("Error generating boxplot: {}", e);
+    }*/
+
+    //chart(vec![vader_sentiment::get_sentiment_value_multiple(NewsArticle::extract_headlines(austria::get_articles(), true)), vader_sentiment::get_sentiment_value_multiple(NewsArticle::extract_headlines(germany::get_articles(), true))], vec!["Austria", "Germany"]);
+    let mut rng = rand::thread_rng();
+    let data_multiple: Vec<Vec<f64>> = (0..4)
+        .map(|_| (0..10).map(|_| rng.gen_range(-1.0..1.0)).collect())
+        .collect();
+
+    let data_single: Vec<f64> = (0..10).map(|_| rng.gen_range(-1.0..1.0)).collect();
+
+    get_chart_single_dataset(data_single, "Austria");
+    get_chart_multiple_dataset_4(data_multiple, vec!["Austria", "Germany", "France", "Czechia"]);
+
+
     //dbg!(austria::get_headlines(false));
-    dbg!(vader_sentiment::get_sentiment_value_multiple(NewsArticle::extract_headlines(austria::get_articles(), true)));
+    //dbg!(vader_sentiment::get_sentiment_value_multiple(NewsArticle::extract_headlines(austria::get_articles(), true)));
     //dbg!(vader_sentiment::get_sentiment_value_single(&String::from("The weather is really bad today")));
     //dbg!(get_sentiment_scores(austria::get_headlines(false)));
     /*generate_wordcloud(austria::get_headlines(false), "austria_wordcloud.png");
